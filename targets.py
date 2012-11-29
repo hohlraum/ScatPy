e76f4af0d7cb0cdaa680f9130331f04c6455ed39
Andrew Mark <mark@is.mpg.de>
2012-11-29 11:42:59 +0100

    Added support classes for decorated glass helix

--- targets.py
+++ targets.py
@@ -272,7 +272,17 @@ class Target_IsoHomo_FROM_FILE(Target):
     def VTRconvert(self, outfile=None):
         """Execute VTRConvert to generate a model file viewable in Paraview"""
         Target.VTRconvert(self, self.fname, outfile)
-        
+
+
+class Target_FROM_FILE(Target_FROM_FILE):
+    pass        
+
+class Target_Iso_FROM_FILE(Target_FROM_FILE):
+    pass        
+
+
+class Target_DecoratedHelix(Target_Iso_FROM_FILE):
+    pass        
 
 class Target_Helix(Target_IsoHomo_FROM_FILE):
     """
